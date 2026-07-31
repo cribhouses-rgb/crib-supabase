b96945871a9b1fc4eba1dcc0c1a94a25350dfb24
cribhouses-rgb <cribhouses@gmail.com>
2026-07-31 13:38:48 +0200

    initial commit: src/main.tsx

---
+++ src/main.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { ErrorBoundary } from "./components/ErrorBoundary";
+import { installGlobalErrorLogging } from "./lib/errorLog";
+import "./index.css";
+
+installGlobalErrorLogging();
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <ErrorBoundary>
+      <App />
+    </ErrorBoundary>
+  </React.StrictMode>
+);
